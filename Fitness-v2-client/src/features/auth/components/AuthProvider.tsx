095ebeae23af9c39563e4148d7abc95314688109
DimRev <dimrev444@gmail.com>
2024-08-31 15:48:58 +0300

    Handle auth provider and add loader

--- Fitness-v2-client/src/features/auth/components/AuthProvider.tsx
+++ Fitness-v2-client/src/features/auth/components/AuthProvider.tsx
@@ -1,48 +1,67 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useLoginFromCookie from "../hooks/useLoginFromCookie";
 import useAuthStore from "../hooks/useAuthStore";
 import useLoginFromSession from "../hooks/useLoginFromSession";
+import { useNavigate } from "react-router-dom";
+import { Loader, LoaderCircle, LoaderPinwheel } from "lucide-react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 function AuthProvider({ children }: Props) {
-  const { mutateAsync: loginFromCookie, isLoading: isLoginFromCookieLoading } =
-    useLoginFromCookie();
-  const {
-    mutateAsync: loginFromSession,
-    isLoading: isLoginFromSessionLoading,
-  } = useLoginFromSession();
-  const { setUser } = useAuthStore();
+  const { mutateAsync: loginFromCookie } = useLoginFromCookie();
+  const { mutateAsync: loginFromSession } = useLoginFromSession();
+  const [isAuthingUser, setIsAuthingUser] = useState(true);
+  const { user, setUser } = useAuthStore();
+  const navigate = useNavigate();
+
   useEffect(() => {
+    // Check if user is already authenticated
+    if (user) {
+      setIsAuthingUser(false);
+      return;
+    }
+
+    const sessionToken = sessionStorage.getItem("fitness_app_session");
+
+    // First attempt to log in from cookie
     void loginFromCookie(undefined, {
       onSuccess: (user) => {
         setUser(user);
+        setIsAuthingUser(false);
       },
-      onError: (err) => {
-        const sessionToken = sessionStorage.getItem("fitness_app_session");
+      onError: () => {
+        // If login from cookie fails, attempt to log in from session
         if (sessionToken) {
           void loginFromSession(
             { session_token: sessionToken },
             {
               onSuccess: (user) => {
                 setUser(user);
+                setIsAuthingUser(false);
               },
-              onError: (err) => {
-                console.error(err);
+              onError: () => {
+                setIsAuthingUser(false);
+                navigate("/");
               },
             },
           );
         } else {
-          console.error(err);
+          setIsAuthingUser(false);
+          navigate("/");
         }
       },
     });
   }, []);
 
-  if (isLoginFromCookieLoading || isLoginFromSessionLoading) {
-    return <div>Loading page...</div>;
+  if (isAuthingUser) {
+    // Show a consistent loading screen or skeleton component
+    return (
+      <div className="flex justify-center items-center w-dvw h-dvh">
+        <LoaderCircle className="text-blue-700 animate-spin size-24" />
+      </div>
+    );
   }
 
   return <>{children}</>;
