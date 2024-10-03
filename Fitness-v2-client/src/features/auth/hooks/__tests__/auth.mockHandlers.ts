9a8ebd7e08fc4b57157c6d8d454fc510573a83cb
DimRev <dimarev444@gmail.com>
2024-10-03 18:57:52 +0300

    chore: Add useRegister tests

--- Fitness-v2-client/src/features/auth/hooks/__tests__/auth.mockHandlers.ts
+++ Fitness-v2-client/src/features/auth/hooks/__tests__/auth.mockHandlers.ts
@@ -70,4 +70,42 @@ export const authHandlers = [
   http.post("*/auth/logout", async () => {
     return HttpResponse.json({ message: "Logged out" }, { status: 200 });
   }),
+
+  http.post("*/auth/register", async ({ request }) => {
+    const { email, password, username } = (await request.json()) as {
+      email: string;
+      password: string;
+      username: string;
+    };
+
+    if (!email || !password || !username) {
+      return HttpResponse.json(
+        { message: "Failed to register, malformed request" },
+        { status: 400 },
+      );
+    }
+
+    if (
+      email === "test@test.com" &&
+      password === "test" &&
+      username === "test"
+    ) {
+      const validUser: AuthUser = {
+        email: "test@test.com",
+        username: "test",
+        image_url: null,
+        role: "user",
+        session_token: "test",
+      };
+
+      return HttpResponse.json(validUser, { status: 200 });
+    }
+
+    if (email === "test_existing@test.com") {
+      return HttpResponse.json(
+        { message: "Failed to register, email already exists" },
+        { status: 409 },
+      );
+    }
+  }),
 ];
