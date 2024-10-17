fd5e0936a158c7757ac4be9e57d1c349e140ab92
DimRev <dimarev444@gmail.com>
2024-10-17 10:53:46 +0300

    fix(client-calendar): Add content to calendar details page, scaffold

--- Fitness-v2-client/src/features/calendar/components/CalendarDetailsContent.tsx
+++ Fitness-v2-client/src/features/calendar/components/CalendarDetailsContent.tsx
@@ -1,10 +1,40 @@
+import { useMemo } from "react";
 import { DashboardContentCards } from "~/features/shared/components/CustomCards";
+import useGetCalendarDataByDate from "../hooks/useGetCalendarDataByDate";
+import { H2 } from "~/features/shared/components/Typography";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+} from "~/features/shared/components/ui/card";
+import { Separator } from "~/features/shared/components/ui/separator";
 
 type Props = {
   dateStr: string;
 };
 
 function CalendarDetailsContent({ dateStr }: Props) {
+  const formattedDate = useMemo(() => {
+    const date = new Date(dateStr);
+    date.setDate(date.getDate() + 1);
+    return date;
+  }, [dateStr]);
+
+  const {
+    data: calendarData,
+    isLoading,
+    error,
+    isError,
+  } = useGetCalendarDataByDate({
+    date: formattedDate,
+  });
+
+  console.log(calendarData);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (isError || !calendarData) return <div>Error: {error?.message}</div>;
+
   return (
     <DashboardContentCards
       title={`Calendar Details - ${new Date(dateStr).toLocaleDateString(
@@ -14,7 +44,61 @@ function CalendarDetailsContent({ dateStr }: Props) {
         },
       )}`}
     >
-      CalendarDetailsContent
+      <Card>
+        <CardHeader>
+          <H2>Meals</H2>
+          <Separator />
+        </CardHeader>
+        <CardContent>
+          {calendarData.meals.length > 0 ? (
+            calendarData.meals.map((meal) => (
+              <div key={meal.meal_id}>{meal.name}</div>
+            ))
+          ) : (
+            <div>No meals</div>
+          )}
+        </CardContent>
+        <CardHeader>
+          <H2>Total Nutritional Values</H2>
+          <Separator />
+        </CardHeader>
+        <CardContent>
+          <div className="w-fit">
+            <div className="grid grid-cols-3 items-center">
+              <span className="text-end">Calories:</span>
+              <div className="mx-5 h-[1px] w-10 bg-foreground" />
+              <span className="">
+                {calendarData.total_calories}
+                <span className="text-xs text-foreground">(kcal)</span>
+              </span>
+            </div>
+            <div className="grid grid-cols-3 items-center">
+              <span className="text-end">Carbs: </span>
+              <div className="mx-5 h-[1px] w-10 bg-foreground" />
+              <span className="">
+                {calendarData.total_carbs}
+                <span className="text-xs text-foreground">(g)</span>
+              </span>
+            </div>
+            <div className="grid grid-cols-3 items-center">
+              <span className="text-end">Fat: </span>
+              <div className="mx-5 h-[1px] w-10 bg-foreground" />
+              <span className="">
+                {calendarData.total_fat}
+                <span className="text-xs text-foreground">(g)</span>
+              </span>
+            </div>
+            <div className="grid grid-cols-3 items-center">
+              <span className="text-end">Protein: </span>
+              <div className="mx-5 h-[1px] w-10 bg-foreground" />
+              <span className="">
+                {calendarData.total_protein}
+                <span className="text-xs text-foreground">(g)</span>
+              </span>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </DashboardContentCards>
   );
 }
