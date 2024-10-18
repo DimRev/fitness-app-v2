6834a93ee0e6c8daa32a15773d4e83ff643b5c94
DimRev <dimarev444@gmail.com>
2024-10-18 14:38:19 +0300

    feat(client-cron):Scaffold Client cron, via Vercel function

---
+++ Fitness-v2-client/api/CustomApiError.ts
@@ -0,0 +1,8 @@
+export default class CustomApiError extends Error {
+  public statusCode: number;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
